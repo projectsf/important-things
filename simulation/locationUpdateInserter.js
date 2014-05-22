6c0af3201ba0e202f6f665f5555973b183f8e614
Scott Maroney <scott.maroney@gmail.com>
2014-05-22 00:10:26 -0700

    better (but not perfect) async behavior.  location displacement now looks correct

--- simulation/locationUpdateInserter.js
+++ simulation/locationUpdateInserter.js
@@ -11,54 +11,52 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
   console.log("connected to db");
+
+  var LocationSchema = mongoose.Schema({
+      created: { type: Date},
+      thingId: { type: String},
+      coords: { 
+        longitude: { type: Number}, 
+        latitude: { type: Number}
+      }
+  });
+
+  //model class
+  var Location = mongoose.model('Location', LocationSchema);
+
+  var numberRecords = 500;
+  var thingId = 'myThing';
+  var initialLocationLatitude = 37.7616535;
+  var initialLocationLongitude= -122.4491129;
+  var maxLocationDisplacementMiles = 1.5;
+  var maxLocationDisplacementDegrees = maxLocationDisplacementMiles / 69.0;
+  var initialTimeStamp = new Date();
+  var locationUpdateIntervalSeconds = 120;
+  var timeStamp = new Date();
+  var timeStampSeconds = initialTimeStamp.getTime();
+  var locationUpdateLatitude = initialLocationLatitude;
+  var locationUpdateLongitude = initialLocationLongitude;
+
+  console.log("generating %d records", numberRecords);
+  for (var i = 0; i < numberRecords; i++) {
+    var location = new Location();
+    timeStamp = new Date(timeStamp.getTime() + locationUpdateIntervalSeconds * 1000);
+    location.created = timeStamp;
+    location.thingId = thingId;
+    location.coords.longitude = locationUpdateLongitude + ((Math.random() - 0.5) * maxLocationDisplacementDegrees);
+    location.coords.latitude = locationUpdateLatitude + ((Math.random() - 0.5) * maxLocationDisplacementDegrees);
+
+    location.save(function (err) {
+      if (err) 
+        return console.error(err, location);
+      }
+    )
+
+  }
+  console.log('finished');
 });
 
-var LocationSchema = mongoose.Schema({
-    created: { type: Date},
-    thingId: { type: String},
-    coords: { 
-      longitude: { type: Number}, 
-      latitude: { type: Number}
-    }
-});
 
-//model class
-var Location = mongoose.model('Location', LocationSchema);
-
-var numberRecords = 50;
-var thingId = 'myThing';
-var initialLocationLatitude = 37.7616535;
-var initialLocationLongitude= -122.4491129;
-var maxLocationDisplacementMiles = 0.5;
-var maxLocationDisplacementDegrees = maxLocationDisplacementMiles / 69.0;
-var initialTimeStamp = new Date();
-var locationUpdateIntervalSeconds = 120;
-var timeStamp = new Date();
-var timeStampSeconds = initialTimeStamp.getTime();
-var locationUpdateLatitude = initialLocationLatitude;
-var locationUpdateLongitude = initialLocationLongitude;
-
-console.log("generating %d records", numberRecords);
-for (var i = 0; i < numberRecords; i++) {
-  locationUpdateLongitude = locationUpdateLongitude + ((Math.random() - 0.5) * maxLocationDisplacementDegrees);
-  locationUpdateLatitude = locationUpdateLatitude + ((Math.random() - 0.5) * maxLocationDisplacementDegrees);
-  timeStamp = new Date(timeStamp.getTime() + locationUpdateIntervalSeconds * 1000);
-
-  //model class instance (document)
-  var location = new Location();
-  location.created = timeStamp;
-  location.thingId = thingId;
-  location.coords.longitude = locationUpdateLongitude;
-  location.coords.latitude = locationUpdateLatitude;
-
-  location.save(function (err) {
-    if (err) 
-      return console.error(err, location);
-    }
-  )
-
-}
-console.log('finished');
 process.on('SIGINT', function() {
   mongoose.connection.close(function () {
     console.log('Mongoose disconnected on app termination');
